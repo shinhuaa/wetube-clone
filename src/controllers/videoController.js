e00412eb5dfa1c80cbb08c5ea42b616d245d329b
shinhuaa <118434093+shinhuaa@users.noreply.github.com>
2023-04-18 21:03:11 +0900

    20230418-2

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -2,28 +2,28 @@
 export const trending = (req, res) => {
   const videos = [
     {
-      title: "hello",
+      title:"Fisrt video",
+      rating: 4.2,
+      comments: 12,
+      createdAt: "2 years ago",
+      views: 5459,
+      id: 1,
     },
     {
-      title: "hello",
+      title:"Second video",
+      rating: 4.5,
+      comments: 22,
+      createdAt: "4 days ago",
+      views: 459,
+      id: 2,
     },
     {
-      title: "hello",
-    },
-    {
-      title: "hello",
-    },
-    {
-      title: "hello",
-    },
-    {
-      title: "hello",
-    },
-    {
-      title: "hello",
-    },
-    {
-      title: "hello",
+      title:"Thrid video",
+      rating: 4.3,
+      comments: 32,
+      createdAt: "7 weeks ago",
+      views: 1324,
+      id: 2,
     },
   ];
   return res.render("home", {pageTitle: "Home", videos});
