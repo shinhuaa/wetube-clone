08c763f22a8b3e266cf3814a9d1d9886fa2a60ac
shinhuaa <118434093+shinhuaa@users.noreply.github.com>
2023-05-19 01:48:21 +0900

    mongoose

--- src/db.js
+++ src/db.js
@@ -1,4 +1,14 @@
 import mongoose from "mongoose";
 
-mongoose.connect("mongodb://127.0.0.1:27017/wetube");
+mongoose.connect("mongodb://127.0.0.1:27017/wetube",{
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
+const db = mongoose.connection;
+
+const handleOpen = () => console.log("connented to DB");
+const handleError = (error) => console.log("fuck", error); 
+
+db.on("error", handleError);
+db.once("open", handleOpen);
