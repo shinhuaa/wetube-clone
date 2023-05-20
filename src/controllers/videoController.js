a499bfacbe17a14595eceae641b4978faee2afbd
shinhuaa <118434093+shinhuaa@users.noreply.github.com>
2023-05-20 21:35:11 +0900

    await

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,67 +1,34 @@
-let videos = [
-  {
-    title:"Fisrt video",
-    rating: 4.2,
-    comments: 12,
-    createdAt: "2 years ago",
-    views: 5459,
-    id: 1,
-  },
-  {
-    title:"Second video",
-    rating: 4.5,
-    comments: 22,
-    createdAt: "4 days ago",
-    views: 459,
-    id: 2,
-  },
-  {
-    title:"Thrid video",
-    rating: 4.3,
-    comments: 32,
-    createdAt: "7 weeks ago",
-    views: 1,
-    id: 3,
-  },
-];
+import Video from "../models/Video"
 
-export const trending = (req, res) => {
-  return res.render("home", {pageTitle: "Home", videos});
+export const home = async(req, res) => {
+  try{
+    const videos = await Video.find({});
+    console.log(videos)
+    return res.render("home", {pageTitle: "Home", videos});
+  } catch{
+    res.render("server-error")
+  }
 }
 export const watch = (req, res) => {
   const { id } = req.params;
-  const video = videos[ id - 1 ];
-  return res.render("watch", {pageTitle: `Watching: ${video.title}`, video});
+  return res.render("watch", {pageTitle: `Watching`});
 }
 export const getEdit = (req, res) => {
   const { id } = req.params;
-  const video = videos[ id - 1 ];
-  return res.render("edit", {pageTitle: `Editing: ${video.title}`, video});
+  return res.render("edit", {pageTitle: `Editing`});
 }
 
 export const postEdit = (req,res) => {
   const { id } = req.params;
   const { title } = req.body;
-  videos[ id - 1 ].title = title;
-  console.log(title);
   return res.redirect(`/videos/${id}`);
 }
 
 export const getUpLoad = (req, res) => {
-  return res.render("upload", {pageTitle: "Uploading Video", videos})
+  return res.render("upload", {pageTitle: "Uploading Video"})
 }
 
 export const postUpLoad = (req, res) => {
-  // here we will add a video to the videos array
   const { title } = req.body;
-  const newVideo = {
-    title,
-    rating: 0,
-    comments: 0,
-    createdAt: "just now",
-    views: 0,
-    id: videos.length + 1,
-  }
-  videos.push(newVideo);
   return res.redirect("/")
 } 
--- src/server.js
+++ src/server.js
@@ -2,14 +2,12 @@
 // npm install --save-dev @babel/core
 // npm install @babel/core @babel/node --save-dev
 //import express from "express"; === const express = require("express"); 왼쪽이 최신 버전 둘다 같음
-import "./db"
 import express from "express";
 import morgan from "morgan";
 import globalRouter from "./routers/globalrouter";
 import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
 
-const PORT = 4000;
 const app = express();
 const logger = morgan("dev");
 
@@ -21,8 +19,7 @@ app.use("/", globalRouter);
 app.use("/videos", videoRouter);
 app.use("/users", userRouter);
 
+export default app;
 
-const handleListening = () => console.log(`Server listening on port http://localhost:${PORT}`)
 
-app.listen(PORT, handleListening)
 
